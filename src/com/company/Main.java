d61465f748dd3873cf194e80055540fc5e10df6a
Andrei1694 <andrei4.stanciu@gmail.com>
2019-07-30 23:54:25 +0300

    finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,17 @@
 package com.company;
 
 import com.company.FileInteraction.FileReader;
+import com.company.FileInteraction.FileWriter;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+        List<String> list = fileReader.readFile();
+        FileWriter fileWriter = new FileWriter();
+        fileWriter.writeNewLine(" *");
+        list.forEach(System.out::println);
     }
 }

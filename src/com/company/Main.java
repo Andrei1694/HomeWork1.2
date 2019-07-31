e21b1c4901d4b053c75a2feef3b6a2f79d0824ec
Andrei1694 <andrei4.stanciu@gmail.com>
2019-07-31 15:25:56 +0300

    finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,11 @@
 package com.company;
 
-import com.company.FileInteraction.FileReader;
-import com.company.FileInteraction.FileWriter;
-
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-        FileReader fileReader = new FileReader();
-        List<String> list = fileReader.readFile();
-        FileWriter fileWriter = new FileWriter();
-        fileWriter.writeNewLine(" *");
-        list.forEach(System.out::println);
+
+        Server server = new Server();
+        server.readFile();
+
     }
 }
--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -1,4 +1,56 @@
 package com.company;
 
+import com.company.FileInteraction.FileReader;
+import com.company.FileInteraction.FileWriter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class Server {
+
+    private FileReader fileReader;
+    private FileWriter fileWriter;
+    private int maxConnections;
+    private List<String> info;
+    private HashMap<String, String> users;
+
+    public Server() {
+        users = new HashMap<>();
+        this.fileReader = new FileReader();
+        this.fileWriter = new FileWriter();
+        this.readFile();
+        this.maxConnections = loadMaxConnections();
+        this.loadUsersAndPasswords();
+    }
+
+    public void readFile() {
+        this.info = this.fileReader.readFile();
+    }
+
+    private int loadMaxConnections() {
+        Optional<String> str = this.info.stream()
+                .filter(e -> e.startsWith("max"))
+                .map(e -> e.replaceAll("\\s+", ""))
+                .findFirst();
+        String s[] = str.get().split("=");
+        int max = Integer.parseInt(s[1]);
+        return max;
+    }
+
+    private void loadUsersAndPasswords() {
+        List<String> bla = this.info.stream()
+                .filter(e -> !e.startsWith("max"))
+                .map(e -> {
+                    String temp[] = e.split(" ");
+                    this.users.put(temp[0].trim(), temp[1].trim());
+                    return e;
+                })
+                .collect(Collectors.toList());
+    }
+
+
+
+
 }
